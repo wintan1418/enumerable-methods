5c67d578c267198fe5fe932ab1db8241b4ef75a2
Arrey Tabe <arrey2391@gmail.com>
2020-11-10 16:00:55 +0100

    implements my_each method

--- enumerable.rb
+++ enumerable.rb
@@ -1,8 +1,21 @@
 module Enumerable
-    def my_each(arr)
-        arr.each {|i| puts 'this is #{} '}
-        
-      # your code here
- 
+
+    
+    def my_each
+       return self.dup unless block_given?
+        i = 0
+        until i == self.size
+            yield  self[i]
+
+            i += 1
+        end
+       self
     end
-  end
+end
+
+test_collect = [1, 10, 100, 1000, 10000, 100000]
+
+
+test_collect.my_each do |el|
+    puts "The test array contains the number #{el}"
+end
