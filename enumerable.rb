b93ae63ea6f8171806ef46298424b2882737ee3e
Arrey Tabe <arrey2391@gmail.com>
2020-11-11 02:46:11 +0100

    implements my_select method

--- enumerable.rb
+++ enumerable.rb
@@ -1,6 +1,12 @@
 module Enumerable
 
-    
+
+
+
+
+=begin
+     #My_each
+=end
     def my_each
        return self.dup unless block_given?
         i = 0
@@ -11,7 +17,7 @@ module Enumerable
         end
        self
     end
-end
+
 
 test_collect = ["goond","young", "lappy", "shangai"]
 
@@ -22,16 +28,46 @@ test_collect.my_each do |el|
     
 end
 
-def my_each_with_index
-  return self.dup unless block_given?
-  i = 0 
-  untill i == self.size
-  yield self[i]
-  i +=1
-end
-end
+=begin
+     #My_each_with_index
+=end 
+
+    def my_each_with_index
+      return self.dup unless block_given?
+        i = 0 
+        until i == self.size
+            yield self[i]
+            i +=1
+        end
+        self
+    end
+
+
+
 test_collect = [23,34,56,78,45]
- test_collect.my_each_with_index  { |el,index| puts "index: #{index} for #{el}" }
+
+ test_collect.my_each_with_index  do |el,index|
+     puts "index: #{index} for #{el}" 
+ 
 end
 
+
+# My_Select
+
+
+    def my_select
+      return self.dup unless block_given?
+        met_cond = []
+        i = 0
+        until i == self.size
+            met_cond << yield(self[i])
+            i +=1
+        end
+         met_cond
+    end
+end
+test_collect = [23,34,56,78,45]
+
+ test_collect.my_select {|num| 
+ p num if num > 30 }
 
